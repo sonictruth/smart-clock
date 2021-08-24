78a738f3351ea16b78495190d44c8bb8ebd76ef2
Alexandru Constantin <dextor@gmail.com>
2021-08-24 14:25:06 +0200

    First

---
+++ README.md
@@ -0,0 +1,5 @@
+# Smart Clock WIP
+
+_Simple digital signage_
+
+App to display time, weather, videos and other things on a tablet-like device. 
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App mono">
+      TEST test 18:00 19:20
+    </div>
+  );
+}
+
+export default App;
