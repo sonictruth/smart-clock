7e66f700449b941a500e942cb5a8757f3ebf756a
Alexandru Constantin <dextor@gmail.com>
2021-08-30 21:00:57 +0200

    Short time

--- src/screens/components/Time.tsx
+++ src/screens/components/Time.tsx
@@ -2,14 +2,10 @@ import './Time.scss';
 import useStore from '../../store';
 
 function Time() {
-    const time = useStore((state: any) => {
-        const date = state.date;
-        return pad(date.getHours()) + ':' + pad(date.getMinutes());
-    });
+    const time = useStore((state: any) => 
+        state.date.toLocaleTimeString(undefined, {timeStyle: 'short'})
+    );
 
-    function pad(number: number) {
-        return String(number).padStart(2, '0');
-    }
     return <div className="Time">
         {time}
     </div>;
