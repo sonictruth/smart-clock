ce05be9fd7f1ed3c265c558b2467a71d29079a05
Alexandru Constantin <dextor@gmail.com>
2021-09-03 16:11:39 +0200

    Change default with no animated ggif

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -3,7 +3,7 @@ import Time from './components/Time';
 import Weather from './components/Weather';
 import { useState, useEffect } from 'react';
 
-const backgrounds = [
+const animatedBackgrounds = [
     'v1.gif',
     'v2.gif',
     'v4.gif',
@@ -13,12 +13,12 @@ const backgrounds = [
     'v13.gif',
 ];
 
-const liteBackgrounds = Array.from(Array(40).keys()).map( index => `d${index+1}.jpeg`);
+const backgrounds = Array.from(Array(40).keys()).map(index => `d${index + 1}.jpeg`);
 
 const backgroundUpdateIntervalSeconds = 600;
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
-const isLite = params.isLite && params.isLite === 'true';
+const isUsingAnimatedBackgrounds = params.animation && params.animation === 'true';
 
 function MainScreen() {
     const [backgroundURL, setBackgroundURL] = useState(getRandomBackground());
@@ -32,10 +32,15 @@ function MainScreen() {
     }, []);
 
     function getRandomBackground() {
-        const collection = isLite ? liteBackgrounds : backgrounds;
+        const collection = isUsingAnimatedBackgrounds ? backgrounds : animatedBackgrounds;
         return collection[Math.floor(Math.random() * collection.length)];
     }
-    return <div style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/backgrounds/${backgroundURL})` }} className="MainScreen">
+    return <div
+        className="MainScreen"
+        style={{
+            backgroundImage:
+                `url(${process.env.PUBLIC_URL}/backgrounds/${backgroundURL})`
+        }}>
         <div className="top" >
             <Time />
         </div>
