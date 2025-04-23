0d7b70c702d9e6314bd19104c61012e1ca309709
Alexandru Constantin <alexandru.constantin@brokergenius.com>
2025-04-23 18:11:22 +0200

    Change weather widget

--- src/screens/components/WeatherExternal.tsx
+++ src/screens/components/WeatherExternal.tsx
@@ -1,36 +1,48 @@
 import { useEffect, useState } from "react";
 import './WeatherExternal.scss';
 
-const refreshInterval = 14400000; // 4 hours in milliseconds
+const widgetHTML = `
+<a class="weatherwidget-io" href="https://forecast7.com/en/41d392d17/barcelona/" data-label_1="BARCELONA" data-label_2="WEATHER" data-font="Arial Black" data-days="3" data-theme="pure" data-basecolor="" data-shadow="#000000" data-accent="" data-textcolor="#ffffff" data-highcolor="#ffffff" data-lowcolor="#ffffff" data-suncolor="#ffce00" data-mooncolor="#b8b8b8" data-cloudcolor="#878787" data-cloudfill="#c0c0c0" data-raincolor="#00d0b8" data-snowcolor="#b1fefa" >BARCELONA WEATHER</a>
+`;
+
+
+const refreshInterval = 19400000; 
 
 function WeatherExternal(props: any) {
     const [reloadKey, setReloadKey] = useState(0);
 
     useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://app3.weatherwidget.org/js/?id=ww_3f3926a5d53e5";
-        script.async = true;
-        document.body.appendChild(script);
-
-        // Cleanup script on component unmount
-        return () => {
-            document.body.removeChild(script);
-        };
+    (function(d: Document, s: string, id: string) {
+        let js: HTMLScriptElement | null = d.getElementById(id) as HTMLScriptElement | null;
+        const fjs = d.getElementsByTagName(s)[0] as HTMLElement | null;
+        if (!js) {
+         js = d.createElement(s) as HTMLScriptElement;
+         js.id = id;
+         js.src = 'https://weatherwidget.io/js/widget.min.js';
+         if (fjs && fjs.parentNode) {
+             fjs.parentNode.insertBefore(js, fjs);
+         }
+        }
+    })(document, 'script', 'weatherwidget-io-js');
+    
+     return () => {
+         const js = document.getElementById('weatherwidget-io-js');
+         if (js && js.parentNode) {
+          js.parentNode.removeChild(js);
+         }
+     };
     }, [reloadKey]); // Re-run effect when reloadKey changes
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setReloadKey((prevKey) => prevKey + 1); // Trigger re-render
+            setReloadKey((prevKey) => prevKey + 1); 
         }, refreshInterval);
 
 
         return () => clearInterval(intervalId);
     }, []);
 
-    const widgetHTML = `
-    <div id="ww_3f3926a5d53e5" v='1.3' loc='id' a='{"t":"responsive","lang":"en","sl_lpl":1,"ids":["wl955"],"font":"Arial","sl_ics":"one_a","sl_sot":"celsius","cl_bkg":"#FFFFFF00","cl_font":"#000000","cl_cloud":"#d4d4d4","cl_persp":"#2196F3","cl_sun":"#FFC107","cl_moon":"#FFC107","cl_thund":"#FF5722","sl_tof":"5","cl_odd":"#00000000"}'>
-    More forecasts: <a href="https://oneweather.org/es/madrid/25_days/" id="ww_3f3926a5d53e5_u" target="_blank">Previsión tiempo 30 días Madrid</a></div>
-    `;
+
 
     return (
         <div key={reloadKey} dangerouslySetInnerHTML={{ __html: widgetHTML }} />
